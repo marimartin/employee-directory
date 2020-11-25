5ad8ad96db16e54a3107efd8692e9bff7b61a544
Mari <marinarosemartin@gmail.com>
2020-11-25 15:01:29 -0600

    imported react and exported all components

---
+++ src/components/DataBody.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DataBody() {
+
+}
+
+export default DataBody;
---
+++ src/components/Searchbox.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SearchBox() {
+
+}
+
+export default SearchBox;
