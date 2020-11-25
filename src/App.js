8da29a50b1440d0b89ff9dbda16b77af3bc02f34
Mari <marinarosemartin@gmail.com>
2020-11-25 15:19:57 -0600

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
 
 import './App.css';
 
 function App() {
   return (
-    
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
   );
 }
 
